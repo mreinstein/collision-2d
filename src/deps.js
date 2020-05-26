ce21754e20b323ea036b264ec93958c20b24a93b
Mike Reinstein <mreinstein@sellpoints.com>
2020-05-26 15:07:36 -0700

    update math external deps to point at new location

--- src/deps.js
+++ src/deps.js
@@ -1,6 +1,6 @@
 import Pool      from 'https://cdn.jsdelivr.net/gh/mreinstein/vec2-gap/pool.js'
-import clamp     from 'https://cdn.jsdelivr.net/gh/mreinstein/math-gap/clamp.js'
-import sign      from 'https://cdn.jsdelivr.net/gh/mreinstein/math-gap/sign.js'
+import clamp     from 'https://cdn.jsdelivr.net/gh/mreinstein/math-gap/src/clamp.js'
+import sign      from 'https://cdn.jsdelivr.net/gh/mreinstein/math-gap/src/sign.js'
 import * as vec2 from 'https://cdn.jsdelivr.net/npm/gl-matrix@3.3.0/esm/vec2.js'
 
 
