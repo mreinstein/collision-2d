8a1fc3c7505b228151b460f0e0b4b1c55fc63c61
Mike Reinstein <mreinstein@sellpoints.com>
2020-05-27 21:47:43 -0700

    fix bug in aabb-segment collision

--- src/aabb-segment-overlap.js
+++ src/aabb-segment-overlap.js
@@ -12,56 +12,59 @@ http://noonat.github.io/intersect/#aabb-vs-segment
 @return bool true if they intersect, false otherwise
 */
 export default function aabbSegmentOverlap (rect, pos, delta, paddingX, paddingY, contact=null) {
-  // if x or y is 0, result will be javascript Infinity
-  let scaleX = 1.0 / delta[0]
-  let scaleY = 1.0 / delta[1]
+    // if x or y is 0, result will be javascript Infinity
+    let scaleX = 1.0 / delta[0]
+    let scaleY = 1.0 / delta[1]
 
-  let signX = sign(scaleX)
-  let signY = sign(scaleY)
-  let halfx = rect.width / 2
-  let halfy = rect.height / 2
-  let posx = rect.position[0] + halfx
-  let posy = rect.position[1] + halfy
-  let nearTimeX = (posx - signX * (halfx + paddingX) - pos[0]) * scaleX
-  let nearTimeY = (posy - signY * (halfy + paddingY) - pos[1]) * scaleY
-  let farTimeX = (posx + signX * (halfx + paddingX) - pos[0]) * scaleX
-  let farTimeY = (posy + signY * (halfy + paddingY) - pos[1]) * scaleY
+    let signX = sign(scaleX)
+    let signY = sign(scaleY)
+    let halfx = rect.width / 2
+    let halfy = rect.height / 2
+    let posx = rect.position[0] + halfx
+    let posy = rect.position[1] + halfy
+    let nearTimeX = (posx - signX * (halfx + paddingX) - pos[0]) * scaleX
+    let nearTimeY = (posy - signY * (halfy + paddingY) - pos[1]) * scaleY
+    let farTimeX = (posx + signX * (halfx + paddingX) - pos[0]) * scaleX
+    let farTimeY = (posy + signY * (halfy + paddingY) - pos[1]) * scaleY
 
-  if (nearTimeX > farTimeY || nearTimeY > farTimeX)
-    return false
+    if (Number.isNaN(nearTimeY))
+        nearTimeY = Infinity
 
-  let nearTime = nearTimeX > nearTimeY ? nearTimeX : nearTimeY
-  let farTime = farTimeX < farTimeY ? farTimeX : farTimeY
+    if (nearTimeX > farTimeY || nearTimeY > farTimeX)
+        return false
 
-  if (nearTime >= 1 || farTime <= 0)
-    return false
+    let nearTime = nearTimeX > nearTimeY ? nearTimeX : nearTimeY
+    let farTime = farTimeX < farTimeY ? farTimeX : farTimeY
 
-  // if we don't have to provide details on the collision, it's sufficient to
-  // return true, indicating the rectangles do intersect
-  if (!contact)
+    if (nearTime >= 1 || farTime <= 0)
+        return false
+
+    // if we don't have to provide details on the collision, it's sufficient to
+    // return true, indicating the rectangles do intersect
+    if (!contact)
+        return true
+
+    /*
+    position is the point of contact between the two objects (or an estimation of it, in some sweep tests).
+    normal is the surface normal at the point of contact.
+    delta is the overlap between the two objects, and is a vector that can be added to the colliding object’s position to move it back to a non-colliding state.
+    time is a fraction from 0 to 1 indicating how far along the line the collision occurred. (This is the t value for the line equation L(t) = A + t * (B - A))
+    */
+    contact.collider = rect
+    contact.time = clamp(nearTime, 0, 1)
+    if (nearTimeX > nearTimeY) {
+        contact.normal[0] = -signX
+        contact.normal[1] = 0
+    } else {
+        contact.normal[0] = 0
+        contact.normal[1] = -signY
+    }
+
+    // NEW
+    contact.delta[0] = (1.0 - contact.time) * -delta[0];
+    contact.delta[1] = (1.0 - contact.time) * -delta[1];
+    contact.position[0] = pos[0] + delta[0] * contact.time;
+    contact.position[1] = pos[1] + delta[1] * contact.time;
+
     return true
-
-  /*
-  position is the point of contact between the two objects (or an estimation of it, in some sweep tests).
-  normal is the surface normal at the point of contact.
-  delta is the overlap between the two objects, and is a vector that can be added to the colliding object’s position to move it back to a non-colliding state.
-  time is a fraction from 0 to 1 indicating how far along the line the collision occurred. (This is the t value for the line equation L(t) = A + t * (B - A))
-  */
-  contact.collider = rect
-  contact.time = clamp(nearTime, 0, 1)
-  if (nearTimeX > nearTimeY) {
-    contact.normal[0] = -signX
-    contact.normal[1] = 0
-  } else {
-    contact.normal[0] = 0
-    contact.normal[1] = -signY
-  }
-
-  // NEW
-  contact.delta[0] = (1.0 - contact.time) * -delta[0];
-  contact.delta[1] = (1.0 - contact.time) * -delta[1];
-  contact.position[0] = pos[0] + delta[0] * contact.time;
-  contact.position[1] = pos[1] + delta[1] * contact.time;
-
-  return true
 }
