b6a12fddc04477be37cb6e97d0afdaf461f63e69
Mike Reinstein <reinstein.mike@gmail.com>
2020-11-07 13:50:25 -0800

    make sphere-sphere overlap test more efficient

--- src/sphere-sphere-overlap.js
+++ src/sphere-sphere-overlap.js
@@ -3,5 +3,5 @@ import { vec2 } from './deps.js'
 
 // @return bool true if the spheres overlap, false otherwise
 export default function sphereSphereOverlap (centerA, radiusA, centerB, radiusB) {
-    return vec2.distance(centerA, centerB) <= (radiusA + radiusB)
+    return vec2.squaredDistance(centerA, centerB) <= ((radiusA + radiusB) ** 2)
 }
