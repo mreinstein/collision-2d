2ef21e5993be7d8a2ddbea9048c260a6b1ca4a99
Mike <reinstein.mike@gmail.com>
2021-02-17 22:32:26 -0800

    initial work on ellipsoid sweep

---
+++ src/segments-ellipsoid-sweep1-indexed.js
@@ -0,0 +1,46 @@
+import TraceInfo      from './TraceInfo.js'
+import { Pool, vec2 } from './deps.js'
+import toji           from './toji-tris.js'
+
+
+const traceInfo = new TraceInfo()
+const p0 = vec2.create()
+const p1 = vec2.create()
+
+
+export default function segmentsSphereEllipsoid1Indexed (lines, indices, lineCount, position, ellipsoid, delta, contact) {
+
+    const endPoint = vec2.add(Pool.malloc(), position, delta)
+
+    traceInfo.resetTrace(position, endPoint, radius)
+
+    let collider = -1
+    for (let i=0; i < lineCount; i++) {
+        const idx = indices[i]
+        const line = lines[idx]
+        const oldT = traceInfo.t
+
+        vec2.divide(p0, line[0], ellipsoid)
+        vec2.divide(p1, line[1], ellipsoid)
+
+        toji.traceSphereTriangle(p0, p1, traceInfo)
+        if (traceInfo.collision && oldT !== traceInfo.t)
+            collider = i
+    }
+
+    if (traceInfo.collision) {
+        contact.time = traceInfo.t
+
+        vec2.copy(contact.position, traceInfo.intersectPoint)
+        vec2.copy(contact.normal, traceInfo.intersectTriNorm)
+
+        vec2.negate(contact.delta, delta)
+        vec2.scale(contact.delta, contact.delta, 1-contact.time)
+
+        contact.collider = collider
+    }
+
+    Pool.free(endPoint)
+
+    return traceInfo.collision
+}
