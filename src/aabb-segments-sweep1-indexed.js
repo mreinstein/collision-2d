a916f5b7bd77c1fe8dcfa7693013692e332e1b6f
Mike <reinstein.mike@gmail.com>
2021-06-06 21:20:37 -0700

    expose contact-copy as a re-usable utility function

--- src/aabb-segments-sweep1-indexed.js
+++ src/aabb-segments-sweep1-indexed.js
@@ -1,20 +1,11 @@
 // based on https://gamedev.stackexchange.com/questions/29479/swept-aabb-vs-line-segment-2d
 import aabbSegmentSweep1 from './aabb-segment-sweep1.js'
 import contact           from './contact.js'
+import copyContact       from './contact-copy.js'
 import { vec2 }          from './deps.js'
 
 
 const _tmpContact = contact()
-
-
-function copyContact (out, contact) {
-    out.time = contact.time
-    out.collider = contact.collider
-    vec2.copy(out.position, contact.position)
-    vec2.copy(out.delta, contact.delta)
-    vec2.copy(out.normal, contact.normal)
-    return out
-}
 
 
 export default function aabbSegmentSweep1Indexed (lines, indices, lineCount, aabb, delta, contact) {
---
+++ src/contact-copy.js
@@ -0,0 +1,11 @@
+import { vec2 } from './deps.js'
+
+
+export default function copyContact (out, contact) {
+    out.time = contact.time
+    out.collider = contact.collider
+    vec2.copy(out.position, contact.position)
+    vec2.copy(out.delta, contact.delta)
+    vec2.copy(out.normal, contact.normal)
+    return out
+}
