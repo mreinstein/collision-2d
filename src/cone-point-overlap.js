afbbd8c8d24eab86093840f83228bd7b945609b8
Mike <reinstein.mike@gmail.com>
2021-10-20 17:02:22 -0700

    add minDistance to cone-point overlap test

--- src/cone-point-overlap.js
+++ src/cone-point-overlap.js
@@ -12,16 +12,21 @@ Determine if a point is within a cone
 @param Vec2 conePosition position of cone origin
 @param number coneRotation observer rotation in radians
 @param number coneFieldOfView angle of cone in degrees
-@param number coneMaxLength  max length of cone
+@param number coneMinDistance  min distance from cone position
+@param number coneMaxDistance  max distance from cone position
 @param Object point  vec2 position of item being checked against cone
 @return Bool true if point is in cone, false otherwise
 */
-export default function conePointOverlap (conePosition, coneRotation, coneFieldOfView, coneMaxLength, point) {
-    if (vec2.distance(conePosition, point) > coneMaxLength)
-      return false
+export default function conePointOverlap (conePosition, coneRotation, coneFieldOfView, coneMinDistance, coneMaxDistance, point) {
+    const dist = vec2.distance(conePosition, point)
+    if (dist > coneMaxDistance)
+        return false
+
+    if (dist < coneMinDistance)
+        return false
 
     if (coneFieldOfView >= 360)
-      return true
+        return true
 
     vec2.set(heading, Math.cos(coneRotation), Math.sin(coneRotation))
     vec2.subtract(delta, point, conePosition)
