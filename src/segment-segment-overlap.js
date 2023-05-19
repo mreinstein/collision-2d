b801c19b2abfa6fe097737980a75034c90b814bc
Mike <reinstein.mike@gmail.com>
2023-05-19 12:14:14 -0700

    make segmentSegmentOverlap return boolean instead of 0 or 1

--- src/segment-segment-overlap.js
+++ src/segment-segment-overlap.js
@@ -2,5 +2,5 @@ import { segseg } from './deps.js'
 
 
 export default function segmentSegmentOverlap (pos1, pos2, pos3, pos4, intersection) {
-    return segseg(intersection, pos1, pos2, pos3, pos4)
+    return segseg(intersection, pos1, pos2, pos3, pos4) === 1
 }
