cbcd5e362513101b065c2a19beb6e1c3aeac4bd5
Mike <reinstein.mike@gmail.com>
2023-08-24 11:58:00 -0700

    fix sphere-sphere-sweep2

--- src/sphere-sphere-sweep2.js
+++ src/sphere-sphere-sweep2.js
@@ -3,24 +3,21 @@ import { vec2 } from './deps.js'
 
 const RESTITUTION = 0.85
 
-const delta = pool.malloc()
-const mtd = pool.malloc()
-const scaled = pool.malloc()
-const v = pool.malloc()
-const normalized = pool.malloc()
-const impulse = pool.malloc()
-const scaledImpulse = pool.malloc()
-
-
-// TODO: this code is totally broken! needs fixing/re-work
+const delta = vec2.create()
+const mtd = vec2.create()
+const scaled = vec2.create()
+const v = vec2.create()
+const normalized = vec2.create()
+const impulse = vec2.create()
+const scaledImpulse = vec2.create()
 
 
 // collide 2 spherical rigid bodies, updating their positions and velocities
 export default function sphereSphereSweep2 (ball1, ball2) {
-    const body1 = ball1.findComponent('rigidbody')
-    const body2 = ball2.findComponent('rigidbody')
+    const body1 = ball1.rigidBody
+    const body2 = ball2.rigidBody
 
-    vec2.subtract(delta, ball1.aabb.position, ball2.aabb.position)
+    vec2.subtract(delta, ball1.transform.position, ball2.transform.position)
 
     const r = body1.radius + body2.radius
 
@@ -48,11 +45,11 @@ export default function sphereSphereSweep2 (ball1, ball2) {
     // push-pull them apart
     // 0 mass means static body unmoved by collisions (doors, machines, etc.)
     if (body1.mass !== 0)
-        vec2.scaleAndAdd(ball1.aabb.position, ball1.aabb.position, mtd, im1 / (im1 + im2))
+        vec2.scaleAndAdd(ball1.transform.position, ball1.transform.position, mtd, im1 / (im1 + im2))
 
     if (body2.mass !== 0) {
         vec2.scale(scaled, mtd, im2 / (im1 + im2))
-        vec2.subtract(ball2.aabb.position, ball2.aabb.position, scaled)
+        vec2.subtract(ball2.transform.position, ball2.transform.position, scaled)
     }
 
     // impact speed
@@ -72,8 +69,6 @@ export default function sphereSphereSweep2 (ball1, ball2) {
     if (body1.mass !== 0)
         vec2.scaleAndAdd(body1.velocity, body1.velocity, impulse, im1)
 
-    if (body2.mass !== 0) {
-        vec2.scale(scaledImpulse, impulse, im2)
-        vec2.subtract(body2.velocity, body2.velocity, scaledImpulse)
-    }
+    if (body2.mass !== 0)
+        vec2.scaleAndAdd(body2.velocity, body2.velocity, impulse, -im2)
 }
